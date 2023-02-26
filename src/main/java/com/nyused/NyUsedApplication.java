7fb1142089cd33b57b34735e92a97b227fb698a0
GuangjieYu1 <yuguangjie1@gmail.com>
2023-02-26 17:40:58 -0500

    Started the backend by using Spring boot framework
    Add the user login and create account functions.

---
+++ src/main/java/com/nyused/NyUsedApplication.java
@@ -0,0 +1,13 @@
+package com.nyused;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyUsedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyUsedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyused/NyUsedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyused;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyUsedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
